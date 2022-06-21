8cc0be9df022cfcbe5ec901a92cb29a7f4160101
Stefan Grubmüller <stefan.grubmueller@polygons.at>
2022-06-21 18:43:23 +0200

    Fix routing and enable routing with firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import {MainPageComponent} from "./main-page/main-page.component";
 import {LogInComponent} from "./log-in/log-in.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/main-page', pathMatch: 'full' },
-  { path: 'main-page', component: MainPageComponent},
-  {path: 'login', component: LogInComponent},
+  { path: '', component: MainPageComponent},
+  { path: 'login', component: LogInComponent},
 ];
 
 @NgModule({
