3185233f28ab27b63d28c7366551280233c101bd
Stefan Grubmüller <stefan.grubmueller@polygons.at>
2022-06-21 20:16:11 +0200

    login check not fully working and also strange behaviour when log in fails

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AuthService} from "../auth-service";
 
 
 @Component({
@@ -9,7 +11,10 @@ import {MatDialogRef} from "@angular/material/dialog";
 })
 export class LogInComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<LogInComponent>) {
+  logedIn: boolean;
+
+  constructor(public dialogRef: MatDialogRef<LogInComponent>, private angularFireAuth: AngularFireAuth, private authService: AuthService) {
+
   }
 
   ngOnInit(): void {
@@ -17,6 +22,31 @@ export class LogInComponent implements OnInit {
 
   close() {
     this.dialogRef.close();
+  }
+
+  logIn(email: string, password: string) {
+    this.authService.loginUser('stefan.grubmueller@icloud.com', 'kul-colours-admin').then((result) => {
+      if (result == null) {                               // null is success, false means there was an error
+        console.log('logging in...');          // when the user is logged in, navigate them to dashboard
+      }
+      else if (result.isValid == false) {
+        console.log('login error', result);
+      }
+      console.log(this.authService.userLoggedIn);
+    });
+
+  }
+
+  logOut() {
+    this.angularFireAuth.signOut();
+  }
+
+  async isLogedIn() {
+    if (this.logedIn === undefined) {
+      this.logedIn = false;
+    }
+    console.log('this.logedIn', this.logedIn);
+    return this.logedIn;
   }
 
 
